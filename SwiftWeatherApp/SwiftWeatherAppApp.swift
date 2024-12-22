027b2d1442cc96dfe2ae19056aff75419fe6bb51
AlcalaJulian <alcala.julian99@gmail.com>
2024-12-22 20:07:51 +0100

    Initial Commit

---
+++ SwiftWeatherApp/SwiftWeatherAppApp.swift
@@ -0,0 +1,17 @@
+//
+//  SwiftWeatherAppApp.swift
+//  SwiftWeatherApp
+//
+//  Created by Julián Alcalá Forero on 22/12/24.
+//
+
+import SwiftUI
+
+@main
+struct SwiftWeatherAppApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
---
+++ SwiftWeatherAppTests/SwiftWeatherAppTests.swift
@@ -0,0 +1,17 @@
+//
+//  SwiftWeatherAppTests.swift
+//  SwiftWeatherAppTests
+//
+//  Created by Julián Alcalá Forero on 22/12/24.
+//
+
+import Testing
+@testable import SwiftWeatherApp
+
+struct SwiftWeatherAppTests {
+
+    @Test func example() async throws {
+        // Write your test here and use APIs like `#expect(...)` to check expected conditions.
+    }
+
+}
